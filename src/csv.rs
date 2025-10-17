e320006e1809a35db3527e245a31bfdbb799148e
Aidan <aidan@aidanrodriguez.com>
2025-10-16 21:55:36 -0700

    expand example yaml

--- src/csv.rs
+++ src/csv.rs
@@ -14,7 +14,7 @@ pub fn collect_fields(line: &str, sep: &str) -> Vec<String> {
 
 fn validate_field(field: &str, spec: &FieldSpec) -> Option<Value> {
     match spec.r#type {
-        DataType::String => {
+        DataType::string => {
             if spec.optional && field == "" {
                 return Some(Value::Null);
             }
